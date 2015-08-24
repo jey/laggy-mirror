fb07abf34fb9850676475ae00d626d93a22b09b7
Jey Kottalam <jey@kottalam.net>
2015-08-24 00:08:54 -0700

    capture audio

--- capture.sh
+++ capture.sh
@@ -1,14 +1,12 @@
+GST_DEBUG=3
 OUTPATH=out.mp4
-DURATION=-1
-BITRATE=8000000
 
-GST_DEBUG=3 gst-launch-1.0 -e \
+GST_DEBUG=$GST_DEBUG gst-launch-1.0 -e \
   uvch264src \
     name=cam \
-    num-buffers=$DURATION \
     auto-start=true \
     usage-type=storage \
-    peak-bitrate=$BITRATE \
+    peak-bitrate=8000000 \
     rate-control=vbr \
     num-reorder-frames=2 \
     entropy=cabac \
@@ -17,11 +15,21 @@ GST_DEBUG=3 gst-launch-1.0 -e \
     ! video/x-h264,width=1920,height=1080,framerate=30/1,profile=high \
     ! h264parse \
     ! tee name=vid \
-  vid. \
     ! queue \
-    ! mp4mux faststart=true \
-    ! filesink location=$OUTPATH \
+    ! outf. \
+  alsasrc device="hw:2,0" \
+    ! audio/x-raw,channels=1 \
+    ! queue \
+    ! audioconvert \
+    ! faac bitrate=128000 \
+    ! audio/mpeg,mpegversion=4,rate=48000,channels=2,base-profile=lc \
+    ! queue2 max-size-buffers=0 max-size-time=0 max-size-bytes=0 \
+    ! outf. \
   vid. \
     ! queue \
     ! vaapidecode \
-    ! vaapisink fullscreen=true
+    ! vaapisink fullscreen=true \
+  mp4mux name=outf faststart=true \
+    ! filesink location=$OUTPATH sync=false \
+  ;
+exit $?
